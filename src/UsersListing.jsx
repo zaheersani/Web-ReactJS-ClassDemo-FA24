e1c4b42d74570527058f37e23f6cd3da1f982534
Zaheer Sani <sani.zaheer@gmail.com>
2024-11-14 23:44:23 +0500

    React Router

--- src/UsersListing.jsx
+++ src/UsersListing.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useParams, Link, NavLink, Outlet } from "react-router-dom";
 
 const Users = () => {
     const [anotherState, setAnotherState] = useState(false)
@@ -30,19 +31,56 @@ const Users = () => {
     // setUsers([])
 
     return (
-        <div>
-            {anotherState ? "True" : "False"}
-            <button onClick={() => setAnotherState(true)}>Update State!</button>
-            <button onClick={() => setLoadUsers(true)}>Load Users!</button>
-            {loadUsers ? "True" : "False"}
-            <ul>
-            {users.map((user, index) => (
-                <li key={user.id}>{index+1}: {user.name}</li>
-            ))}
-            </ul>
-        </div>
+        <>
+            <div>
+                {anotherState ? "True" : "False"}
+                <button onClick={() => setAnotherState(true)}>Update State!</button>
+                <button onClick={() => setLoadUsers(true)}>Load Users!</button>
+                {loadUsers ? "True" : "False"}
+                <ul>
+                {users.map((user, index) => (
+                    <NavLink to={`/users/${user.id}`} >
+                    <li key={user.id}>{index+1}: {user.name}</li>
+                    </NavLink>
+                ))}
+                </ul>
+            </div>
+            <Outlet />
+        </>
     );
 
 }
 
+
+const UserDetails = () => {
+    const { id } = useParams();
+    const [user, setUser] = useState(null);
+
+    console.log('User ID:', id)
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const data = await res.json();
+            setUser(data);
+        };
+        fetchUsers();
+    }, [])
+
+    console.log(user)
+
+    if(!user) {
+        return <div>Loading...</div>
+    }
+
+    return (
+        <div>
+            <h1>User Details</h1>
+            <p>Username: {user.username}</p>
+            <p>Email: {user.email}</p>
+        </div>
+    )
+}
+
+export {UserDetails};
 export default Users;
